fd96750268d63cbda5921ec7e6cc0fde4b69c91f
Omri Harel <omriharel@users.noreply.github.com>
2016-05-15 00:59:17 +0300

    introduce text

--- app/js/directives/mdsCard.js
+++ app/js/directives/mdsCard.js
@@ -7,18 +7,15 @@ function MdsCard() {
             song: '=',
             type: '@'
         },
-        link: (scope, element, attrs) => {
+        link: (scope, element) => {
+            scope.typeDependent = {
+                'now-playing': {flex: '66', hr: 'Now Playing', css: 'mds-card-now-playing'},
+                'up-next': {flex: '33', hr: 'Up Next', css: 'mds-card-up-next'},
+                undefined: {flex: '25', css: 'mds-card-normal'}
+            }[scope.type];
             element.addClass('layout-column');
-            element.addClass('flex-' + (function () {
-                switch (scope.type) {
-                    case 'now-playing':
-                        return '66';
-                    case 'up-next':
-                        return '33';
-                    default:
-                        return '25';
-                }
-            })());
+            element.addClass('flex-' + scope.typeDependent.flex);
+            element.addClass(scope.typeDependent.css);
         }
     };
 }
